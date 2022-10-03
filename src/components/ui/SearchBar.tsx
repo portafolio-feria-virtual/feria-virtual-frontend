503b72fbf433e11c8965ab6b6690b081af14def8
Milenko Tomasevic <milenkotomasevic41@gmail.com>
2022-10-02 22:57:58 -0300

    searchbar

---
+++ src/components/ui/SearchBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+        <form>   
+            <div className="relative">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </div>
+                <input type="search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" required/>
+                <button type="submit" className="text-grey absolute right-2.5 bottom-2.5 bg-white-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Buscar</button>
+            </div>
+        </form>
+  )
+}
+
+export default SearchBar
