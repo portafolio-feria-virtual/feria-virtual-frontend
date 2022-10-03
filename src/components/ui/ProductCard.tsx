115158884a6286de7ef738a464ecaf809ca1d802
Milenko Tomasevic <milenkotomasevic41@gmail.com>
2022-10-02 23:35:05 -0300

    Creacion de componentes recomendados

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProductCard() {
   return (
     
-    <div className="w-full max-w-sm bg-white rounded-lg shadow-md border">
+    <div className="w-full max-w-sm bg-white rounded-lg shadow-lg border">
         <img className="p-8 rounded-t-lg" src="https://www.65ymas.com/uploads/s1/23/51/17/naranjas-beneficios-contraindicaciones-y-recomendaciones-con-ellas.jpeg" alt="product image"/>
         <div className="px-5 pb-5 ">
             <h5 className="text-xl font-semibold tracking-tight text-gray-900">Naranjas por kilo</h5>
