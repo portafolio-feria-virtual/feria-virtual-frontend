58424d54f0cbf4a79b3baf678b87704bd96a7c74
Milenko Tomasevic <milenkotomasevic41@gmail.com>
2022-10-02 23:19:11 -0300

    button-color-changed

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function SearchBar() {
+export default function SearchBar() {
   return (
         <form>   
             <div className="relative">
@@ -8,10 +8,8 @@ function SearchBar() {
                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
                 <input type="search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" required/>
-                <button type="submit" className="text-grey absolute right-2.5 bottom-2.5 bg-white-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Buscar</button>
+                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-gray-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Buscar</button>
             </div>
         </form>
   )
 }
-
-export default SearchBar
