289246f35fa091407afef515af3c9d278fe025fe
Milenko Tomasevic <milenkotomasevic41@gmail.com>
2022-10-02 23:18:51 -0300

    productcard-added

--- src/components/dashboards/dashboardCI/DashboardCI.tsx
+++ src/components/dashboards/dashboardCI/DashboardCI.tsx
@@ -1,4 +1,5 @@
 import { useUsers } from '../../../hooks/useUsers';
+import ProductCard from '../../ui/ProductCard';
 import SearchBar from '../../ui/SearchBar';
 
 const DashboardCI = () => {
@@ -7,14 +8,18 @@ const DashboardCI = () => {
   return (
     <div className="">
 
-      <div>
+      <div className="my-10">
         <SearchBar/>
       </div>
 
       <h2>Recomendados</h2>
 
-      <div>
-        
+      <div className="mx-20 my-10 grid gap-5 grid-cols-1 md:grid-cols-5">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
 
     </div>
