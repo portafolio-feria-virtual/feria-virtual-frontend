6008b7bbeab9423c44ed5f59fb5e63cea80a7665
Milenko Tomasevic <milenkotomasevic41@gmail.com>
2022-10-09 15:09:45 -0300

    Refactorizacion de interfaz

--- src/components/dashboards/dashboardCI/DashboardCI.tsx
+++ src/components/dashboards/dashboardCI/DashboardCI.tsx
@@ -1,49 +1,54 @@
 import { useUsers } from '../../../hooks/useUsers';
-import { ExtendedCard, Filters, FullCard, ProductCard, ProductDetail, SearchBar, SellerDetail } from '../../marketplace';
+import { IProductData } from '../../../interfaces/marketplace.interface';
+import {
+  ExtendedCard,
+  Filters,
+  FullCard,
+  ProductCard,
+  SearchBar
+} from '../../marketplace';
 
-const data = {
-  title: "Naranjas por kilo",
-  price: 3000,
-  img: "https://www.65ymas.com/uploads/s1/23/51/17/naranjas-beneficios-contraindicaciones-y-recomendaciones-con-ellas.jpeg",
-  stock: 100
-}
-
+const data: IProductData[] = [
+  {
+    id: '1',
+    title: 'Naranjas por kilo',
+    price: 3000,
+    img: 'https://www.65ymas.com/uploads/s1/23/51/17/naranjas-beneficios-contraindicaciones-y-recomendaciones-con-ellas.jpeg',
+    stock: 100
+  }
+];
 
 const DashboardCI = () => {
   const { logout, user } = useUsers();
 
   return (
     <div className="">
-
       <div className="my-10">
-        <SearchBar/>
+        <SearchBar />
       </div>
 
       <h2>Recomendados</h2>
 
       <div className="mx-20 my-10 grid gap-5 grid-cols-1 md:grid-cols-5">
-        <ProductCard productData={data}/>
-        <ProductCard productData={data}/>
-        <ProductCard productData={data}/>
-        <ProductCard productData={data}/>
-        <ProductCard productData={data}/>
+        {data.map(product => (
+          <ProductCard key={product.id} {...product} />
+        ))}
       </div>
 
       <div className="mx-20 my-10 grid gap-10 grid-cols-2">
         <div>
-          <ExtendedCard productData={data}/>
-          <ExtendedCard productData={data}/>
-          <ExtendedCard productData={data}/>
+          {data.map(product => (
+            <ExtendedCard key={product.id} {...product} />
+          ))}
         </div>
         <Filters />
       </div>
 
       <div className="mx-20 my-10">
-        <FullCard productData={data}/>
-        <ProductDetail />
-        <SellerDetail />
+        {data.map(product => (
+          <FullCard key={product.id} {...product} />
+        ))}
       </div>
-
     </div>
   );
 };
